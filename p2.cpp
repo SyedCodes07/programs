6fdd7e2ff69640c52fc0b49e15be40bbb4ba4c1d
SyedCodes07 <nb25vdt045.syed@nmit.ac.in>
2026-09-24 12:53:15 +0530

    Add program 1.2

---
+++ p2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string s;
+  cout<<"Enter word:";
+cin>>s;
+cout<<"length: "<<s.length()<<endl;
+cout<<"Upper:";
+
+    return 0;
+}
+ 
